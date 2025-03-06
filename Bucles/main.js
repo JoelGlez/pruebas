c9d03f76bd1cfca28a0fd41acaae83fb51be7f69
JoelGlez <145745663+JoelGlez@users.noreply.github.com>
2025-03-05 22:45:58 -0600

    Add files via upload

---
+++ Bucles/main.js
@@ -0,0 +1,22 @@
+for(let i=0;i<=10;i++){
+    console.log("con for"+ i);
+
+} 
+
+/*
+let contador=1
+while (contador<=10){
+    console.log("con for"+ contador);
+    contador++
+}
+
+*/
+
+
+let contador=10
+do{
+    console.log("con while se a imprimir una vez este bien ")
+    contador++
+}
+
+while (contador<=10);
